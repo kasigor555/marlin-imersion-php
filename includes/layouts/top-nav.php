1ef5875e7e59a26b90799750c256b4e0c0669bb0
Igor Kas <kasigor555@gmail.com>
2020-04-04 20:41:12 +0300

    Поправлены ссылки в top-menu

--- includes/layouts/top-nav.php
+++ includes/layouts/top-nav.php
@@ -15,17 +15,17 @@ $user = new User;
         </li>
         <?php if ($user->hasPermissions('admin')) { ?>
           <li class="nav-item">
-            <a class="nav-link" href="admin/index.php">Управление пользователями</a>
+            <a class="nav-link" href="/admin/">Управление пользователями</a>
           </li>
         <?php } ?>
       </ul>
 
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a href="profile.php" class="nav-link">Профиль</a>
+          <a href="/profile.php" class="nav-link">Профиль</a>
         </li>
         <li class="nav-item">
-          <a href="logout.php" class="nav-link">Выйти</a>
+          <a href="/logout.php" class="nav-link">Выйти</a>
         </li>
       </ul>
     </div>
@@ -35,16 +35,16 @@ $user = new User;
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Главная</a>
+          <a class="nav-link" href="/">Главная</a>
         </li>
       </ul>
 
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a href="login.php" class="nav-link">Войти</a>
+          <a href="/login.php" class="nav-link">Войти</a>
         </li>
         <li class="nav-item">
-          <a href="register.php" class="nav-link">Регистрация</a>
+          <a href="/register.php" class="nav-link">Регистрация</a>
         </li>
       </ul>
     </div>
